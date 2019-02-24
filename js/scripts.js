88298628b6c8926c233572c9b362b8a395c5b587
Jason Campbell <43692594+jcampbell18@users.noreply.github.com>
2019-02-23 16:37:08 -0800

    Updated scripts.js
    Copied from CodePen

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,61 @@
 console.log('OK');
 
 // Your Awesome Scripts
+// LetterJS
+$('h1').lettering();
+
+// Animation
+$('#stage').hover( function() {
+  
+  // slide
+  $('h1').toggleClass('slide-h1');
+  // rotate
+  $('svg#guitar').toggleClass('rotate-guitar');
+  // skew
+  $('img#satch').toggleClass('skew-satch');
+  // opacity (hide)
+  $('#google').toggleClass('opacity-zero');
+  // misc
+  $('svg.music-notes').toggleClass('music-notes-transition');
+  
+  $('keyframes').hover();
+  
+});
+
+// Audio play button
+var isPlaying = false;
+$('#play-btn').click( function() { 
+    if (!isPlaying) {
+        isPlaying = true;
+        // play music
+        $('#play-music').get(0).play();
+        // change play button to pause
+        $('#play-btn').attr('src', '../img/pause_button.jpg');
+        // add background of album
+        $('#swta').attr('src', '../img/SurfingWithTheAlienAlbumCover.jpg');
+        // remove guitar. music lines and notes
+        $('svg#music-lines').addClass('opacity-zero');
+        $('svg#guitar').addClass('opacity-zero');
+        $('svg#music-note-two').addClass('opacity-zero');
+        $('svg#music-note-three').addClass('opacity-zero');
+        $('svg#music-note-four').addClass('opacity-zero');
+        $('svg#music-note-five').addClass('opacity-zero');
+        $('svg#music-note-six').addClass('opacity-zero');
+        $('svg#google').css('opacity', '0');
+        $('img#satch').css('opacity', '0');
+    } else {
+        isPlaying = false;
+        $('#play-music').get(0).pause();
+        $('#play-btn').attr('src', '../img/play_button.jpg');
+        $('#swta').attr('src', '');
+        $('svg#music-lines').removeClass('opacity-zero');
+        $('svg#guitar').removeClass('opacity-zero');
+        $('svg#music-note-two').removeClass('opacity-zero');
+        $('svg#music-note-three').removeClass('opacity-zero');
+        $('svg#music-note-four').removeClass('opacity-zero');
+        $('svg#music-note-five').removeClass('opacity-zero');
+        $('svg#music-note-six').removeClass('opacity-zero');
+        $('svg#google').css('opacity', '1');
+        $('img#satch').css('opacity', '1');
+    }
+});
